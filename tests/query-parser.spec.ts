ed58573ea476f6c00f441fde7b23157ce072f559
khaledosama999 <khaledosama52@gmail.com>
2021-03-26 18:47:39 +0200

    test(operators): update the tests to use the new query parser class

---
+++ tests/query-parser.spec.ts
@@ -0,0 +1,101 @@
+import { expect } from 'chai';
+
+import AlgoliaQueryParser from '../src/query-parser';
+
+describe.only('query parser', () => {
+  it('eq', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { eq: 3 },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field = 3');
+  });
+
+  it('gt', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { gt: 3 },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field > 3');
+  });
+
+  it('gte', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { gte: 3 },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field >= 3');
+  });
+
+  it('lt', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { lt: 3 },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field < 3');
+  });
+
+  it('lte', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { lte: 3 },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field <= 3');
+  });
+
+  it('between', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { between: [1, 2] },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field: 1 TO 2');
+  });
+
+  it('not', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { not: { eq: 3 } },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('(NOT field = 3)');
+  });
+
+  it('or', () => {
+    const queryParser = new AlgoliaQueryParser({
+      or: [
+        { field1: { eq: 3 } },
+        { field2: { eq: 4 } },
+      ],
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('( field1 = 3 OR field2 = 4 )');
+  });
+
+  it('and', () => {
+    const queryParser = new AlgoliaQueryParser({
+      and: [
+        { field1: { eq: 3 } },
+        { field2: { eq: 4 } },
+      ],
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field1 = 3 AND field2 = 4');
+  });
+
+  it('in', () => {
+    const queryParser = new AlgoliaQueryParser({
+      field: { in: [1, 2] },
+    });
+
+    const query = queryParser.exec();
+    expect(query).to.eq('field = 1 AND field = 2');
+  });
+});
